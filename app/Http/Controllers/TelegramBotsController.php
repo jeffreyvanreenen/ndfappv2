d82a2d563836ed3f3f11041f8b15fe291f90922b
Jeffrey van Reenen <jeffrey92.hrb@gmail.com>
2022-04-25 20:53:00 +0200

    Initial commit

--- app/Http/Controllers/TelegramBotsController.php
+++ app/Http/Controllers/TelegramBotsController.php
@@ -58,6 +58,16 @@ class TelegramBotsController extends Controller
 
     public function webhook()
     {
-        
+        $path = "https://api.telegram.org/bot5307159749:AAEiJzmNfY_sKqP8hH2Y8R2V2y5a7IBftSY";
+        $update = json_decode(file_get_contents("php://input"), TRUE);
+
+        $chatId = $update["message"]["chat"]["id"];
+        $message = $update["message"]["text"];
+
+        if (strpos($message, "/weather") === 0) {
+            $location = substr($message, 9);
+            $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
+            file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+        }
     }
 }
