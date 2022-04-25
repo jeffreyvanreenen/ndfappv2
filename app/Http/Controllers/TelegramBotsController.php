fac5cb2dec6d1797caaf2dd87ae2d115c7974b40
Jeffrey van Reenen <jeffrey92.hrb@gmail.com>
2022-04-25 22:45:42 +0200

    Initial commit

--- app/Http/Controllers/TelegramBotsController.php
+++ app/Http/Controllers/TelegramBotsController.php
@@ -62,7 +62,7 @@ class TelegramBotsController extends Controller
     public function webhook(Request $request)
     {
 //        $path = "https://api.telegram.org/bot5307159749:AAEiJzmNfY_sKqP8hH2Y8R2V2y5a7IBftSY";
-//        $response = Http::get('https://api.telegram.org/bot5307159749:AAEiJzmNfY_sKqP8hH2Y8R2V2y5a7IBftSY/sendmessage?chat_id=-737197277&text=' . $request);
+        $response = Http::get('https://api.telegram.org/bot5307159749:AAEiJzmNfY_sKqP8hH2Y8R2V2y5a7IBftSY/sendmessage?chat_id=-737197277&text=Hallo!');
 
         $chat = TelegramChat::updateOrCreate(
             [
