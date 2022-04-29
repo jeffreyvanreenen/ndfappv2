826ee8e864b39c2ae02585744b563799398f3f5c
Jeffrey van Reenen <jeffrey92.hrb@gmail.com>
2022-04-29 10:01:12 +0200

    Initial commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\GenerateRssScrapeJob;
+use App\Jobs\TwitterScrapeJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +18,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->job(new TwitterScrapeJob)->everyFifteenMinutes();
+        $schedule->job(new GenerateRssScrapeJob)->everyFifteenMinutes();
     }
 
     /**
