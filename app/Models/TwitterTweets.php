9a9c6d75d00f599a50b2f4b327df57f83631dd4d
Jeffrey van Reenen <jeffrey92.hrb@gmail.com>
2022-05-01 22:30:30 +0200

    Initial commit

--- app/Models/TwitterTweets.php
+++ app/Models/TwitterTweets.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class TwitterTweets extends Model
 {
+    use Searchable;
+    
     protected $fillable = [
         'tw_id',
         'author_id',
